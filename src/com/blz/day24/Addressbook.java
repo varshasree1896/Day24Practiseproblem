ff8c7de94ac8ad42290c334d66660f47972fd107
Pooja Sree <you@example.com>
2022-04-15 14:55:06 +0530

    Added welcome message

---
+++ src/com/blz/day24/Addressbook.java
@@ -0,0 +1,12 @@
+package com.blz.day24;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class Addressbook {
+
+    public static void main(String[] args) {
+
+    }
+}
